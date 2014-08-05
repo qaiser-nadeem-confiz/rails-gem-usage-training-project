9ed0c4204e362714ae575263321fef8a9de61626
Qaiser Nadeem <Qaiser.nadeem@confiz.com>
2014-08-05 12:55:14 +0500

    Gem training project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '2f7fd85f22ad61d19a7df63d862556546de12ae87e353ca96f735a23c67759e0d99643706534480d4c5749788f829de0a321fb6ff6abd79dcaa4d483ed651e69'
